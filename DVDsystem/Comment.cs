a3a3b6a4f37244a00dbdc9aa1d507950041a216a
xl <34930726+xl2486982302@users.noreply.github.com>
2018-01-07 11:34:46 +0800

    登录遮罩层

---
+++ DVDsystem/Comment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wujing
+{
+    public class Comment
+    {
+        public string pname { get; set; }
+        public string pprice { get; set; }
+        public string pcateid { get; set; }
+        public string pnum { get; set; }
+        public string pmanufacturer { get; set; }
+        public string pavailability { get; set; }
+        public string pdescription { get; set; }
+        public string pmode { get; set; }
+        public string pintroduction { get; set; }
+        public string filesname { get; set; }
+        public string pID { get; set; }
+        public string cID { get; set; }
+        public string cname { get; set; }
+        public string ucar { get; set; }
+        public string oID { get; set; }
+        public string cmail { get; set; }
+        public string cphone { get; set; }
+        public string ccity { get; set; }
+        public string ccountry { get; set; }
+        public string caddress { get; set; }
+        public string cfullname { get; set; }
+ 
+    }
+}
---
+++ DVDsystem/Hander/yzheng.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="yzheng.ashx.cs" Class="wujing.yzheng" %>
