3805425c635156271e5fb597fe32350330383016
xl <34930726+xl2486982302@users.noreply.github.com>
2018-01-08 23:18:37 +0800

    MD5加密

--- DVDsystem/Hander/index.ashx.cs
+++ DVDsystem/Hander/index.ashx.cs
@@ -6,6 +6,7 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Web.SessionState;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace DVDsystem.Hander
 {
@@ -19,31 +20,38 @@ namespace DVDsystem.Hander
         {
             context.Response.ContentType = "text/html";
             string search = context.Request["keyword"];
-
-            HttpCookie user;
             string username = "";
             string username2 = "";
-
-            if (context.Request["username"] != null)//第一次加载请求必定为空.
-            {
-                if (context.Session[context.Request["username"]] != null)//已经
-                    username = context.Request["username"];
-            }
-            else
-            {
-                user = context.Request.Cookies["C005-7D5B-4231-8CEA-1693"];//不知道什么原因，之前取出来的cookie值的@变成了%40
+            
+            HttpCookie user = context.Request.Cookies["C005-7D5B-4231-8CEA-1693"];//不知道什么原因，之前取出来的cookie值的@变成了%40
                 if (user != null)
                 {
                     username2 = user.Value.ToString();
 
                     if (context.Session[username2] != null && !context.Session[username2].ToString().Equals(""))
                         username = user.Value.ToString();
-
                 }
+                
+                else if()
             }
-        }
+      
+    private string Md5jiami (string encryptString)
 
-        public bool IsReusable
+    {
+
+        byte[] result = Encoding.Default.GetBytes(encryptString);
+
+        MD5 md5 = new MD5CryptoServiceProvider();
+
+        byte[] output = md5.ComputeHash(result);
+
+        string encryptResult = BitConverter.ToString(output).Replace("-", "");
+
+        return encryptResult;
+
+    }
+
+    public bool IsReusable
         {
             get
             {
